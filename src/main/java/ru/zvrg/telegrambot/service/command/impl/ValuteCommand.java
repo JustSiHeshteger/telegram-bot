d096f3dc14af7d690bed968692ecda6a6e4ee287
DalekiyM4rs <evgeniybulychev@bk.ru>
2024-03-25 11:32:11 +0300

    Add ability to write dollar price and today date. Tried to create file with today json content, couse don't want to parse file each time, but something went wrong. Need discussion

--- src/main/java/ru/zvrg/telegrambot/service/command/impl/ValuteCommand.java
+++ src/main/java/ru/zvrg/telegrambot/service/command/impl/ValuteCommand.java
@@ -3,12 +3,17 @@ package ru.zvrg.telegrambot.service.command.impl;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import ru.zvrg.telegrambot.dto.Valute;
 import ru.zvrg.telegrambot.listener.TelegramBotListener;
 import ru.zvrg.telegrambot.service.ValuteService;
 import ru.zvrg.telegrambot.service.command.DefaultCommand;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.List;
 
 @Component
 @AllArgsConstructor
@@ -20,12 +25,25 @@ public class ValuteCommand implements DefaultCommand {
     @Override
     public void executeCommand(Update update, TelegramBotListener telegramBotListener) throws IOException {
         var list = valuteService.getValuteFromCbr();
-        //FIXME убрать потом
-        String usd = "USD";
+        try {
+            telegramBotListener.execute(getAnswer(update, list, "USD"));
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private SendMessage getAnswer(Update update, List<Valute> list, String currentValute) {
         var u = list.stream()
-                .filter(valute -> valute.getCharCode().equals(usd))
+                .filter(valute -> valute.getCharCode().equals(currentValute))
                 .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("Не найдено нужной валюты = " + usd));
+                .orElseThrow(() -> new IllegalArgumentException("Не найдено нужной валюты = " + currentValute));
         log.info("найденная валюта {}", u);
+        String answer = LocalDate.now() + "\nFound valute: " + u.getCharCode() + " - " + u.getName() + "\nCurrent price: " + u.getValue()
+                        + " RUB to " + u.getNominal() + " " + u.getCharCode();
+        //update.getMessage().getChat().getUserName();
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(update.getMessage().getChatId()));
+        message.setText(answer);
+        return message;
     }
 }
