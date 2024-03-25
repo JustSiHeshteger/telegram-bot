156e19532f5428723b6351d7c9ad6e3495faac22
DalekiyM4rs <evgeniybulychev@bk.ru>
2024-03-25 11:32:11 +0300

    Add ability to write dollar price and today date. Tried to create file with today json content, couse don't want to parse file each time, but something went wrong. Need discussion

--- src/main/java/ru/zvrg/telegrambot/service/JSONService.java
+++ src/main/java/ru/zvrg/telegrambot/service/JSONService.java
@@ -1,27 +1,26 @@
 package ru.zvrg.telegrambot.service;
-
 import org.springframework.stereotype.Service;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.URL;
-
 @Service
 public class JSONService {
 
     public String getJSONFile(String link) throws IOException {
+        StringBuilder jsonContent;
         final URL url = new URL(link);
         BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
-        StringBuilder jsonContent = new StringBuilder();
+        jsonContent = readFromFile(reader);
+        return jsonContent.toString();
+    }
 
+    public StringBuilder readFromFile(BufferedReader reader) throws IOException {
+        StringBuilder jsonContent = new StringBuilder();
         String line;
         while ((line = reader.readLine()) != null) {
             jsonContent.append(line);
         }
-
         reader.close();
-
-        return jsonContent.toString();
+        return jsonContent;
     }
 }
