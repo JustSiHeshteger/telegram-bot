e6b35327ef425062a11bcd0da90456785e28d116
DalekiyM4rs <evgeniybulychev@bk.ru>
2024-03-28 12:33:15 +0300

    Переделан вывод, поженились с master

--- src/main/java/ru/zvrg/telegrambot/service/command/impl/ValuteCommand.java
+++ src/main/java/ru/zvrg/telegrambot/service/command/impl/ValuteCommand.java
@@ -24,7 +24,7 @@ public class ValuteCommand implements DefaultCommand {
 
     @Override
     public void executeCommand(Update update, TelegramBotListener telegramBotListener) throws IOException {
-        var list = valuteService.getValuteFromCbr();
+        final var list = valuteService.getValuteFromCbr();
         try {
             telegramBotListener.execute(getAnswer(update, list, "USD"));
         } catch (TelegramApiException e) {
@@ -33,15 +33,15 @@ public class ValuteCommand implements DefaultCommand {
     }
 
     private SendMessage getAnswer(Update update, List<Valute> list, String currentValute) {
-        var u = list.stream()
+        final var u = list.stream()
                 .filter(valute -> valute.getCharCode().equals(currentValute))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("Не найдено нужной валюты = " + currentValute));
         log.info("найденная валюта {}", u);
-        String answer = LocalDate.now() + ": " + "\n" + u.getValue() + " " + u.getCharCode() +
+        final String answer = LocalDate.now() + ": " + "\n" + u.getValue() + " " + u.getCharCode() +
                          " - " + u.getNominal() + " RUB";
         //update.getMessage().getChat().getUserName();
-        SendMessage message = new SendMessage();
+        final SendMessage message = new SendMessage();
         message.setChatId(String.valueOf(update.getMessage().getChatId()));
         message.setText(answer);
         return message;
